c8cf211920e692502851dc334447f91233f23bd9
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-01-13 19:02:35 +0100

    headeer style

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,16 +1,32 @@
-<header>
-    <a href="/">
-        <img src="{{ asset('images/dc-logo.png') }}" alt="Logo">
-    </a>
-    <nav>
-        <ul>
-            @foreach ($menu as $item)
-            <li>
-                <a href="{{ route($item['route']) }}">
-                    {{ $item['label'] }}
-                </a>
-            </li>
-            @endforeach
-        </ul>
-    </nav>
+<header class="flex-row-center">
+    <div class="container flex-row-center">
+        <div class="logo">
+            <a href="/">
+                <img src="{{ asset('images/dc-logo.png') }}" alt="Logo">
+            </a>
+        </div>
+        <nav>
+            <ul>
+                @foreach ($menu as $item)
+                <li>
+                    <a href="{{ route($item['route']) }}">
+                        {{ $item['label'] }}
+                    </a>
+                </li>
+                @endforeach
+            </ul>
+        </nav>
+    </div>
+    <div class="container flex-row-center">
+        <div class="header-search flex-row-center">
+            <button class="flex-row-center">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </div>
+        <div class="header-login">
+            <button>SIGN UP</button>
+            <button>LOG IN</button>
+        </div>
+    </div>
+
 </header>
