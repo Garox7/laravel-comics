deeb15d22c8a27378d588045bfb3517d86716b10
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-01-14 16:29:02 +0100

    badges section style

---
+++ resources/views/guest/partials/badges.blade.php
@@ -0,0 +1,26 @@
+<section class="badges">
+    <div class="container">
+        <div class="badges-container flex-row-center">
+          <div class="badges">
+            <img src="{{ asset('images/buy-comics-digital-comics.png') }}" alt="Digital Comics">
+            <h4>digital comics</h4>
+          </div>
+          <div class="badges">
+            <img src="{{ asset('images/buy-comics-merchandise.png') }}" alt="Comics Merchandise">
+            <h4>dc merchandise</h4>
+          </div>
+          <div class="badges">
+            <img src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="Comics Subscriptions">
+            <h4>subscriptions</h4>
+          </div>
+          <div class="badges">
+            <img src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="Shop Locator">
+            <h4>comic shop locator</h4>
+          </div>
+          <div class="badges">
+            <img src="{{ asset('images/buy-dc-power-visa.svg') }}" alt="Power Visa">
+            <h4>dc power visa</h4>
+          </div>
+        </div>
+    </div>
+  </section>
