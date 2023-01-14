156bf1a31a908e6f24f28cf8d376aa0fad62984f
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-01-14 17:09:48 +0100

    footer style

---
+++ resources/views/guest/partials/footer.blade.php
@@ -0,0 +1,67 @@
+<footer>
+    <div class="footer__top" style="background-image: url({{ asset('images/footer-bg.jpg') }})">
+        <div class="footer__top__container">
+            <div class="footer__link">
+                <div class="link">
+                    <ul>
+                        <h3>dc comics</h3>
+                        <li><a href="#!">Characters</a></li>
+                        <li><a href="#!">Comics</a></li>
+                        <li><a href="#!">Movies</a></li>
+                        <li><a href="#!">Tv</a></li>
+                        <li><a href="#!">Games</a></li>
+                        <li><a href="#!">Videos</a></li>
+                        <li><a href="#!">News</a></li>
+                    </ul>
+                    <ul>
+                        <h3>shop</h3>
+                        <li><a href="#!">Shop DC</a></li>
+                        <li><a href="#!">Shop DC Collectables</a></li>
+                    </ul>
+                </div>
+                <div class="link">
+                    <ul>
+                        <h3>dc</h3>
+                        <li><a href="#!">Term Of Use</a></li>
+                        <li><a href="#!">Privaxy Polici</a></li>
+                        <li><a href="#!">Ad Choise</a></li>
+                        <li><a href="#!">Advertising</a></li>
+                        <li><a href="#!">Jobs</a></li>
+                        <li><a href="#!">Subscription</a></li>
+                        <li><a href="#!">Talent workshop</a></li>
+                        <li><a href="#!">CPSC Certificates</a></li>
+                        <li><a href="#!">Ratings</a></li>
+                        <li><a href="#!">Shop Help</a></li>
+                        <li><a href="#!">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="link">
+                    <ul>
+                        <h3>sites</h3>
+                        <li><a href="#!">DC</a></li>
+                        <li><a href="#!">MAD magazine</a></li>
+                        <li><a href="#!">DC Kids</a></li>
+                        <li><a href="#!">DC Universe</a></li>
+                        <li><a href="#!">DC Power Visaå</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer__logo">
+                <img src="{{ asset('images/dc-logo-bg.png') }}" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="footer__bottom">
+        <div class="footer__bottom__container">
+            <button>sign-up now!</button>
+            <div class="footer__social">
+                <h2>follow us</h2>
+                <img src="{{ asset('images/footer-facebook.png') }}" alt="Facebook">
+                <img src="{{ asset('images/footer-twitter.png') }}" alt="Twitter">
+                <img src="{{ asset('images/footer-youtube.png') }}" alt="You Tube">
+                <img src="{{ asset('images/footer-pinterest.png') }}" alt="Pinterest">
+                <img src="{{ asset('images/footer-periscope.png') }}" alt="Periscope">
+            </div>
+        </div>
+    </div>
+</footer>
